34d5cfefb2861bff2a5351ce5d3e1bce71856381
Brittany Walker <70984129+Britt22walk@users.noreply.github.com>
2020-12-04 12:46:55 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ function getRecipes(query, dietFilter){
         app_id: appId,
         app_key: apiKey,
         to: totalResults,
-        health: dietFilter
+    
     };
+
+    if(dietFilter){
+        params.health = dietFilter
+
+    }
     let queryString=formatQuery(params)
     let url=recipeUrl + '?' + queryString;
     console.log(url);
@@ -36,19 +41,70 @@ function formatQuery(params){
 
 function displayRecipes(responseJson){
     console.log(responseJson)
+    let ingredients=responseJson.hits[i].recipe.ingredientLines.map((a)=>{
+        return`<div>
+        <h3>Ingredients</h3>
+        <ul id="js-ingredients">
+        <li>${a}</li>
+        </ul>
+        < /div>`;
+    })
     for(let i=0; i<responseJson.hits.length; i++){
-        $('#js-results').append(`<div><h3>${responseJson.hits.recipe[i].label}</h3>
-        <img src='${responseJson.hits.recipe[i].image}' width='300px'></div>` 
+        $('#js-results').append(`<div><h3>${responseJson.hits[i].recipe.label}</h3>
+        <img src="${responseJson.hits[i].recipe.image}" class="child"> 
+        <div>
+          <ul>
+          <li>Serving Size:</li>
+          <li>Diet Labels:${responseJson.hits[i].recipe.healthLabels}</li>
+          </ul>
+        </div>
+        <div>
+        ${ingredients.join('')}
+        </div>` 
         )};
     $('#js-results').removeClass('hidden');
 }
 
+/*
+      <section class="child">
+        <h3>Beer Recomendations based of your recipe....</h3>
+        <div>
+          <h4>Beer Rec #1</h4>
+          <img src="gnocchi.jpg" alt="gnocchi" width="50%">
+          <p>"This straight-up US style pale ale uses some of our <br>
+              favourite hops against a toasty malt base. We brewed this <br>
+            with Sir Tom Hunter at our Fraserburgh brewery - to add a <br>
+            charitable element to your own version, feel free to share <br>
+            this citrusy pale ale with your friends. Or don't, it's your choice."</p>
+        </div>
+        <div>
+          <h4>Beer Rec #2</h4>
+          <img src="gnocchi.jpg" alt="gnocchi" width="50%">
+          <p>"This straight-up US style pale ale uses some of our <br>
+            favourite hops against a toasty malt base. We brewed this <br>
+            with Sir Tom Hunter at our Fraserburgh brewery - to add a <br>
+            charitable element to your own version, feel free to share <br>
+            this citrusy pale ale with your friends. Or don't, it's your choice."</p>
+        </div>
+        <div>
+          <h4>Beer Rec #3</h4>
+          <img src="gnocchi.jpg" alt="gnocchi" width="50%">
+          <p>"This straight-up US style pale ale uses some of our <br>
+            favourite hops against a toasty malt base. We brewed this <br>
+            with Sir Tom Hunter at our Fraserburgh brewery - to add a <br>
+            charitable element to your own version, feel free to share <br>
+            this citrusy pale ale with your friends. Or don't, it's your choice."</p>
+        </div>`)
+    })
+}*/
+
 function watchForm(){
     $('form').submit(event =>{
         event.preventDefault();
+        $('#js-results').empty();
         let keywordSearchTerm=$('#js-keyword-search').val();
-        let dietFilter=$('input[type="checkbox"]').val();
-        console.log("Searching for " + dietFilter);
+        let dietFilter=$('input[type="checkbox"]:checked').val();
+        console.log("Searching for " + keywordSearchTerm);
         getRecipes(keywordSearchTerm, dietFilter);
     }) 
 }
